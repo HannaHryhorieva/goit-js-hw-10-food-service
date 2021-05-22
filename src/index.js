26b5dd442985386d34708f761bedfa9b7519664a
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-05-23 01:21:45 +0300

    переключатель сделан

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './sass/main.scss';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const switchToggle = document.querySelector('#theme-switch-toggle');
+const bodyEl = document.querySelector('body');
+
+
+function populateToggle() {
+    const valueCheck = localStorage.getItem('themeValue');
+      if (valueCheck) {
+      bodyEl.classList.add(valueCheck);  
+    } else {
+        bodyEl.classList.add(Theme.LIGHT);
+      localStorage.setItem('themeValue', Theme.LIGHT)  
+    };
+    if (valueCheck === Theme.DARK) {
+        switchToggle.setAttribute('checked', 'true')
+    };
+};
+populateToggle();
+
+switchToggle.addEventListener('change', onChangeTheme);
+
+function onChangeTheme(evt) {
+if (bodyEl.classList.contains(Theme.LIGHT)) {
+    bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('themeValue', Theme.DARK);
+    switchToggle.setAttribute('checked', 'true')
+} else {
+    bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('themeValue', Theme.LIGHT);
+    switchToggle.removeAttribute('checked');
+    };   
+};
